5acf0481a7c9fe05f0f79e06c846e46cd19ed7c2
superzbo <zbo2@qq.com>
2016-04-25 23:03:56 +0800

    rabbit

---
+++ src/main/java/org/zbo/done/amqp/FooListenser.java
@@ -0,0 +1,13 @@
+package org.zbo.done.amqp;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zbo on 4/25/16.
+ */
+@Component
+public class FooListenser {
+    public void listen(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ src/test/java/org/zbo/done/amqp/RabbitMQTests.java
@@ -0,0 +1,25 @@
+package org.zbo.done.amqp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by zbo on 4/25/16.
+ */
+@ContextConfiguration(locations = {"classpath:spring/business-config.xml",
+                                   "classpath:spring/rabbitmq-config.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RabbitMQTests {
+
+    @Autowired
+    RabbitTemplate template;
+
+    @Test
+    public void rabbitmq_send_receive_test() {
+        template.convertAndSend("myExchange", "foo.bar", "Hello, world!");
+    }
+}
